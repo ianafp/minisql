84e4e5d363d47f9222ac82b75785b605be379c6a
ianafp <WoDeJinLun@outlook.com>
2022-05-31 23:11:23 +0800

    fix a little bug

--- src/page/table_page.cpp
+++ src/page/table_page.cpp
@@ -187,6 +187,10 @@ bool TablePage::GetFirstTupleRid(RowId *first_rid) {
 
 bool TablePage::GetNextTupleRid(const RowId &cur_rid, RowId *next_rid) {
   ASSERT(cur_rid.GetPageId() == GetTablePageId(), "Wrong table!");
+  if(cur_rid.GetPageId()!=GetTablePageId()){
+    next_rid->Set(INVALID_PAGE_ID, 0);
+    return false;
+  }
   // Find and return the first valid tuple after our current slot number.
   for (auto i = cur_rid.GetSlotNum() + 1; i < GetTupleCount(); i++) {
     if (!IsDeleted(GetTupleSize(i))) {
